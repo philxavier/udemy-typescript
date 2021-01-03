86f65c77a2209e5666628ee765a4ed1787d78ca3
Felipe Lima <fsxlima@gmail.com>
2021-01-03 17:28:19 -0500

    finish

--- src/app.js
+++ src/app.js
@@ -46,3 +46,15 @@ function useVehicle(vehicle) {
         vehicle.loadCargo(1000);
     }
 }
+function moveAnimal(animal) {
+    var speed;
+    switch (animal.type) {
+        case "bird":
+            speed = animal.flyingSpeed;
+            break;
+        case "horse":
+            speed = animal.runningSpeed;
+    }
+    console.log("Moving at speed: " + speed);
+}
+moveAnimal({ type: "bird", flyingSpeed: 10 });
